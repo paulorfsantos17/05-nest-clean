f73d419919f25bc6fd871b6b12397149a9f08200
Paulo <paulorfsantos17@gmail.com>
2024-08-01 16:54:31 -0300

    refactor: change  repositories of  interface for abstract class

--- src/domain/forum/application/repositories/answer-comments-repository .ts
+++ src/domain/forum/application/repositories/answer-comments-repository .ts
@@ -1,12 +1,13 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-export interface AnswerCommentRepository {
-  findById(id: string): Promise<AnswerComment | null>
-  findManyByAnswerId(
+export abstract class AnswerCommentRepository {
+  abstract findById(id: string): Promise<AnswerComment | null>
+  abstract findManyByAnswerId(
     answerId: string,
     params: PaginationParams,
   ): Promise<AnswerComment[]>
-  create(questionComment: AnswerComment): Promise<void>
-  delete(questionComment: AnswerComment): Promise<void>
+
+  abstract create(questionComment: AnswerComment): Promise<void>
+  abstract delete(questionComment: AnswerComment): Promise<void>
 }
